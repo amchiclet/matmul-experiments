c2114c0b3d47df6c7c1650de7b0a133f98f284b5
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2024-09-28 17:37:56 -0500

    update openmp pragmas

--- matmul_omp/program_01/program_01_de/core.c
+++ matmul_omp/program_01/program_01_de/core.c
@@ -14,12 +14,13 @@ extern int N;
 
 int core(double (*restrict A)[M][K], double (*restrict B)[K][N], double (*restrict C)[M][N]) {
 
-#pragma omp parallel for private(i, j, k)
-  for (int i = 0; i <= M - 1; i += 1) {
+  int i, j, k;
+  #pragma omp parallel for private(i, j, k)
+  for (i = 0; i <= M - 1; i += 1) {
     double sum = 0.0;
-    for (int j = 0; j <= N - 1; j += 1) {
+    for (j = 0; j <= N - 1; j += 1) {
       sum = 0;
-      for (int k = 0; k <= K - 1; k += 1) {
+      for (k = 0; k <= K - 1; k += 1) {
         sum = sum + (*A)[i][k] * (*B)[k][j];
       }
       (*C)[i][j] = (*C)[i][j] + sum;
