aa1376c75fa5e9b54a822c1a60766e1bf20a8af0
Amarin Phaosawasdi <phaosaw2@illinois.edu>
2024-11-10 11:52:41 -0600

    make sure mkl and omp print pid

--- matmul_mkl/program_01/program_01_de/wrapper.c
+++ matmul_mkl/program_01/program_01_de/wrapper.c
@@ -89,7 +89,7 @@ void measure(int n_iterations, int inputs[16]) {
   char filename[64];
   snprintf(filename, 64, "codelet_info_%ld.txt", (long)getpid());
   FILE *f;
-  f = fopen("codelet_info.txt", "w");
+  f = fopen(filename, "w");
   fprintf(f, "# n_iterations M N K cpu_time_ns\n");
   fprintf(f, "%d %d %d %d %" PRIu64 "\n", n_iterations, M, N, K, cpu_time_ns);
   fclose(f);
